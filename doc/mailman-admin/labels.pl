801a96287ac2d8ee23b64c59df298e5027982051
Mark Sapiro <mark@msapiro.net>
2016-08-26 22:29:34 -0700

    Rebuilt docs.

--- doc/mailman-admin/labels.pl
+++ doc/mailman-admin/labels.pl
@@ -1,17 +1,9 @@
-# LaTeX2HTML 2002-2-1 (1.71)
+# LaTeX2HTML 2008 (1.71)
 # Associate labels original text with physical files.
 
 
 $key = q/general-personality/;
 $external_labels{$key} = "$URL/" . q|node10.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/sender-filters/;
-$external_labels{$key} = "$URL/" . q|node22.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/about/;
-$external_labels{$key} = "$URL/" . q|node36.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/front/;
@@ -22,19 +14,27 @@ $key = q/contents/;
 $external_labels{$key} = "$URL/" . q|node2.html|; 
 $noresave{$key} = "$nosave";
 
-1;
-
-
-# LaTeX2HTML 2002-2-1 (1.71)
-# labels from external_latex_labels array.
-
-
-$key = q/general-personality/;
-$external_latex_labels{$key} = q|2.1|; 
+$key = q/about/;
+$external_labels{$key} = "$URL/" . q|node36.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/sender-filters/;
+$external_labels{$key} = "$URL/" . q|node22.html|; 
+$noresave{$key} = "$nosave";
+
+1;
+
+
+# LaTeX2HTML 2008 (1.71)
+# labels from external_latex_labels array.
+
+
+$key = q/sender-filters/;
 $external_latex_labels{$key} = q|2.7|; 
+$noresave{$key} = "$nosave";
+
+$key = q/general-personality/;
+$external_latex_labels{$key} = q|2.1|; 
 $noresave{$key} = "$nosave";
 
 1;
