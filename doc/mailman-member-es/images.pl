8e8f1b6b7ddb65f59b18a02fd7ca7b834a3d5179
Mark Sapiro <mark@msapiro.net>
2020-01-11 17:19:27 -0800

    Rebuild docs for release.

--- doc/mailman-member-es/images.pl
+++ doc/mailman-member-es/images.pl
@@ -2,12 +2,6 @@
 # Associate images original text with physical files.
 
 
-$key = q/nomath_inline}textexclamdownnomath_inline};MSF=1.6;AAT/;
-$cached_env_img{$key} = q|<IMG
- WIDTH="6" HEIGHT="19" ALIGN="MIDDLE" BORDER="0"
- SRC="|."$dir".q|img2.png"
- ALT="\textexclamdown">|; 
-
 $key = q/>;MSF=1.6;AAT/;
 $cached_env_img{$key} = q|<IMG
  WIDTH="17" HEIGHT="29" ALIGN="MIDDLE" BORDER="0"
@@ -19,6 +13,12 @@ $cached_env_img{$key} = q|<IMG
  WIDTH="8" HEIGHT="19" ALIGN="MIDDLE" BORDER="0"
  SRC="|."$dir".q|img1.png"
  ALT="\textquestiondown">|; 
+
+$key = q/nomath_inline}textexclamdownnomath_inline};MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="6" HEIGHT="19" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img2.png"
+ ALT="\textexclamdown">|; 
 
 $key = q/<;MSF=1.6;AAT/;
 $cached_env_img{$key} = q|<IMG
